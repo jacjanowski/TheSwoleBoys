d066b88f9a9e885db15c0e538dafbfd9e4894c29
jacjanowski <jjanow7@uic.edu>
2021-03-31 13:45:37 -0500

    add new css page for tips. add slideshow.

--- stylesheets/model.js
+++ stylesheets/model.js
@@ -22,30 +22,30 @@
 //     }
 // }
 $('ul').on('click', 'li', function () {
-    $(this).toggleClass("completed");
+	$(this).toggleClass("completed");
 });
 
 //click on X to delete 'todo'
 $('ul').on('click', 'span', function (event) {
-    $(this).parent().fadeOut(1000, function () {
-        $(this).remove();
-    });
-    event.stopPropagation();
+	$(this).parent().fadeOut(1000, function () {
+		$(this).remove();
+	});
+	event.stopPropagation();
 
 });
 
 $("input[type='text']").keypress(function (event) {
-    if (event.which === 13) {
-        var todoText = $(this).val();
-        $(this).val("");
-        //create a new 'li' and add to 'ul'
-        $('ul').append('<li><span><i class="fa fa-trash"></i></span>' + todoText + '</li>');
+	if (event.which === 13) {
+		var todoText = $(this).val();
+		$(this).val("");
+		//create a new 'li' and add to 'ul'
+		$('ul').append('<li><span><i class="fa fa-trash"></i></span>' + todoText + '</li>');
 
-    }
+	}
 });
 
 $('.fa-plus').click(function () {
-    $("input[type='text']").fadeToggle();
+	$("input[type='text']").fadeToggle();
 });
 
 
